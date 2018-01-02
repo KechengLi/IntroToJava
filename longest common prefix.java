0cbf8e3d38114a617058501de2da6a440310b840
Kecheng Li <kecheng485@gmail.com>
2018-01-02 18:12:44 -0500

    initial lcp

---
+++ longest common prefix.java
@@ -0,0 +1,21 @@
+public static class longest common prefix
+{
+
+private static String lcp(String s, String t)
+{
+    int N = Math.min(s.length(), t.length());
+    for (int i = 0; i < N; i++)
+    {
+        if (s.charAt(i) != t.charAt(i))
+            return s.substring(0, i);
+    }
+    return s.substring(0, N);
+}
+
+public static void main(String args[])
+{
+    //test client for lcp
+    
+}
+
+}
